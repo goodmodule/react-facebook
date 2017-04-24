4b7d3a3b07402d6e59375a9e2883351036bafacd
Zlatko Fedor <zfedor@gmail.com>
2017-04-24 15:02:38 +0200

    deps propTypes

--- src/CommentsCount.jsx
+++ src/CommentsCount.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import Parser from './Parser';
 import getCurrentHref from './utils/getCurrentHref';
 
--- src/Parser.jsx
+++ src/Parser.jsx
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import canUseDOM from 'can-use-dom';
 import FacebookProvider from './FacebookProvider';
 
