2bca75423271dbb9d17741b2ca856d8f627b71c8
Zlatko Fedor <zfedor@gmail.com>
2017-06-01 10:47:08 +0200

    added missing file

---
+++ src/utils/clearUndefinedProperties.js
@@ -0,0 +1,16 @@
+export default function clearUndefinedProperties(obj) {
+  if (!obj) {
+    return obj;
+  }
+
+  const newObj = {};
+
+  Object.keys(obj).forEach(propertyName => {
+    const value = obj[propertyName];
+    if (value !== undefined) {
+      newObj[propertyName] = value;
+    }
+  });
+
+  return newObj;
+}
